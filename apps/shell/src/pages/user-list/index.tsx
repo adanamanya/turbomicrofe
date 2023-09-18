39e21921a2c9d598268d6496566ab8271d7e9c0f
adanamanya <adanamanyaa@gmail.com>
2023-09-18 11:34:52 +0700

    add lottie loading

--- apps/shell/src/pages/user-list/index.tsx
+++ apps/shell/src/pages/user-list/index.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 import { ReactElement, Suspense } from 'react'
 
+import Lottie from 'lottie-react'
 import dynamic from 'next/dynamic'
 
+import loadingAnim from '../../../public/assets/image/loading.json'
 import useUserData from '../../hooks/pages/UserList/useUserData'
 // Wrap the dynamic import in a no-ssr component
 const RemoteTable = dynamic(() => import('@/apps/user-list/src/pages/index'), {
@@ -12,15 +14,25 @@ import MainLayout from '@/layouts/MainLayout'
 const UserList = () => {
   const { users, loading, error } = useUserData()
   if (loading) {
-    return <div>Loading...</div>
+    // Use Lottie animation here
+    return (
+      <div className="flex items-center justify-center w-screen">
+        <Lottie animationData={loadingAnim} /> {/* Replace with your animation data */}
+      </div>
+    )
   }
-
   if (error) {
     return <div>Error: {error.message}</div>
   }
   return (
     <div className="card w-full">
-      <Suspense fallback={<p>Loading...</p>}>
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center w-screen">
+            <Lottie animationData={loadingAnim} /> {/* Replace with your animation data */}
+          </div>
+        }
+      >
         <RemoteTable userDataTable={users} />
       </Suspense>
     </div>
